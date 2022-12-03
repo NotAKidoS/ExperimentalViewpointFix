7dbd2f362a7652b6e6b91170fc05a71f5b454acd
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2022-12-03 01:57:33 -0600

    why

---
+++ ExperimentalViewpointFix/Main.cs
@@ -0,0 +1,19 @@
+﻿using ABI_RC.Core.Player;
+using HarmonyLib;
+using MelonLoader;
+
+namespace ExperimentalViewpointFix;
+
+public class ExperimentalViewpointFix : MelonMod
+{
+    [HarmonyPatch]
+    private class HarmonyPatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerSetup), "SetViewPointOffset")]
+        private static void PlayerSetup_SetViewPointOffset_Postfix(bool isCalibrate, ref bool __runOriginal)
+        {
+            if (!isCalibrate) __runOriginal = false;
+        }
+    }
+}
---
+++ ExperimentalViewpointFix/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using ExperimentalViewpointFix.Properties;
+using MelonLoader;
+using System.Reflection;
+
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(ExperimentalViewpointFix))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(ExperimentalViewpointFix))]
+
+[assembly: MelonInfo(
+    typeof(ExperimentalViewpointFix.ExperimentalViewpointFix),
+    nameof(ExperimentalViewpointFix),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+
+namespace ExperimentalViewpointFix.Properties;
+internal static class AssemblyInfoParams
+{
+    public const string Version = "1.0.0";
+    public const string Author = "NotAKidoS";
+}
